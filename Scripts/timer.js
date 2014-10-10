64ced1d2342b2c4a0b48fa0dfa3e6b0747eb236e
waltertan12 <waltertan12@gmail.com>
2014-10-10 10:41:28 -0400

    Timer Update
    Changed count method exclusively to setTimeout. Added input boxes to
    page.

--- Scripts/timer.js
+++ Scripts/timer.js
@@ -3,41 +3,77 @@ $(document).ready(function() {
 	var run = false;
 
 	//initialize time to 0
-	var seconds = 0;
+	var sec = 0;
+	var min = 0;
+	var hours = 0;
 
 	//Start timer, run count function
 	$('.start').on('click', function(){
 		run = true;
+
 		//Some retarded testing thing I was doing when I got frustrated...
-		$('.test').text(run);
-		count();
+		$('.test').text("Start lol");
+
+		count()
 	});
 
 	//Stop timer
 	$('.stop').on('click', function(){
 		run = false;
+
 		//Some retarded testing thing I was doing when I got frustrated...
-		$('.test').text(run);
+		$('.test').text("Stop heuheuheuhe");
+
 	});
 
 	//Reset timer, set seconds back to zero and stop timer
 	$('.reset').on('click', function(){
 		run = false;
-		seconds = 0;
-		$('.timer').text(seconds);
-		$('.test').text(run);
+		//Reset time
+		sec = 0;
+		min = 0;
+		hours = 0;
+
+		$('.timer').text(formatTime(hours) + " : " + formatTime(min) + " : " + formatTime(sec));
+				count();
+		$('.test').text("Reset :(");
+
 	});
+
+	//Format time to always have two digits e.g. 00:00:05 instead of 0:0:5
+	function formatTime(val){
+		if (val < 10){
+			return "0" + val;
+		}
+		return val;
+	}
 
 	//count up from zero
 	function count(){
-			/**setInterval(function, x) calls the function every x milliseconds until
-			clearInterval is called**/
-			setInterval(function(){
-				//Only run if start is clicked
-				if(run){
-					$('.timer').html(++seconds);
+		/**setTimeout(function, x) runs the function x millliseconds 
+		after being called**/
+		setTimeout(function(){
+
+			//Only run if start is clicked
+			if(run){
+
+				//run timer
+				sec += 1;
+				if (sec == 60){
+					sec = 0;
+					min += 1;
 				}
-			},1000);
+				if (min == 60) {
+					min = 0;
+					hours += 1;
+				}
+				$('.timer').text(formatTime(hours) + " : " + formatTime(min) + " : " + formatTime(sec));
+				
+				//Repeat
+				count();
+			}
+
+		},1000);
 	}
 
 });
