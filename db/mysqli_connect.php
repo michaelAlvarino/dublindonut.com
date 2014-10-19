bc1af698ee3f5cb8927339ad778aba6dc91b6ed6
Michael Alvarino <michael.alvarino@gmail.com>
2014-10-19 19:16:08 -0400

    fix some links

--- db/mysqli_connect.php
+++ db/mysqli_connect.php
@@ -1,5 +1,10 @@
 <?
-	$db = new mysqli('pdb18.atspace.me', '1733679_sb', 'logmeindude12', '1733679_sb');
+	declare('DB_HOST','pdb18.atspace.me');
+	declare('DB_USER','1733679_sb');
+	declare('DB_PASS','logmeindude12')
+	declare('DB_NAME','poem')
+
+	$db = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 	if($db->connect_errno){
 		die('Sorry, we\'re having some problems...');
 	}
