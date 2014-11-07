acde1036aa59fdce496db45c41b68be473a1ab61
Michael Alvarino <michael.alvarino@gmail.com>
2014-11-06 22:55:06 -0500

    removed pw, added try catch to poemInfo.php

--- db/poemInfo.php
+++ db/poemInfo.php
@@ -3,7 +3,12 @@ include_once("mysqli_connect.php");
 	
 	if(isset($_POST['poem'])){
 		$poem=mysql_real_escape_string($_POST['poem']);
-		mysqli_query($conn,"INSERT INTO Poem (poemSubTime,poem) VALUES (NOW(),'".$poem."')");
+		try{
+			mysqli_query($conn,"INSERT INTO Poem (poemSubTime,poem) VALUES (NOW(),'".$poem."')");	
+		}catch(Exception $e){
+			echo "Woops... something went wrong";
+		}
+		
 		mysqli_close($conn);
 		print $poem;
 
