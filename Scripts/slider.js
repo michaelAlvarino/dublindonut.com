fdccdc34e6aed128dc83829d661ccd00fcc76a3e
Walter Tan <waltertan12@gmail.com>
2014-11-09 15:30:23 -0500

    slider

---
+++ Scripts/slider.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+
+
+	$('.nextSlide').on('click',function() {
+
+		var currentSlide = $('.active-slide');
+		var nextSlide = currentSlide.next();
+
+
+		if(nextSlide.length === 0) {
+      		nextSlide = $('.slide').first();
+    	}
+
+		currentSlide.fadeOut(500).removeClass('active-slide');
+		nextSlide.fadeIn(500).addClass('active-slide');
+	
+
+	});
+
+	$('.prevSlide').on('click',function(){
+
+		var currentSlide = $('.active-slide');
+		var prevSlide = currentSlide.prev();
+
+
+		if(prevSlide.length === 0) {
+      		prevSlide = $('.slide').last();
+    	}
+
+		currentSlide.fadeOut(500).removeClass('active-slide');
+		prevSlide.fadeIn(500).addClass('active-slide');
+
+	})
+
+
+})
